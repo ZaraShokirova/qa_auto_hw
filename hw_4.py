dd6ed10fe1e3f44acf705fcbc8f9e19b9241e97d
zarina.shokirova <zarina.shokirova@alif.tj>
2024-03-04 17:45:29 +0500

    Домашняя работа к 4 уроку

---
+++ hw_4.py
@@ -0,0 +1,21 @@
+# def test_greeting():
+#     name = "Анна"
+#     age = 25
+#     output = f"Привет, {name}! Тебе {age} лет."
+#     print(output)
+#     assert output == "Привет, Анна! Тебе 25 лет."
+
+# def test_rectangle():
+#     """
+#     Напишите программу, которая берет длину и ширину прямоугольника
+#     и считает его периметр и площадь.
+#     """
+#     a = 10
+#     b = 20
+#     # TODO сосчитайте периметр
+#     perimeter = 2 * (a + b)
+#     print(perimeter)
+#     assert perimeter == 60
+#     area = a * b
+#     assert area == 200
+
